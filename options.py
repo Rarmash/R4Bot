ff74154dea16e2339d4ed60e180091e1614186e8
Andrew <62994051+Rarmash@users.noreply.github.com>
2024-06-15 15:20:55 +0300

    Hotfix for v1.1

--- options.py
+++ options.py
@@ -7,7 +7,7 @@ from modules.generateConfig import *
 # Load environment variables from .env file
 load_dotenv()
 
-version = "1.1"
+version = "1.1.1"
 
 # Get environment variables
 token = environ.get('TOKEN')
