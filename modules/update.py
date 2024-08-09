1cb5cb2c930fd152ab92509dcabeb92b21ba7631
Andrew <62994051+Rarmash@users.noreply.github.com>
2024-08-10 02:22:49 +0300

    Git pull fixes

--- modules/update.py
+++ modules/update.py
@@ -6,33 +6,24 @@ import time
 
 def update_and_restart():
     update_dir = f"update"
-
-    print(f"Основной каталог: {os.getcwd()}")
-
     try:
         curr_path = os.getcwd()
-
-        print(f"Текущий рабочий каталог: {curr_path}")
 
         for root, dirs, files in os.walk(update_dir):
             for file in files:
                 src_path = os.path.join(root, file)
                 rel_path = os.path.relpath(src_path, update_dir)
-                dest_path = os.path.join(curr_path, rel_path)  # Используем base_dir
+                dest_path = os.path.join(curr_path, rel_path)
 
                 print(f"Перемещение: {src_path} -> {dest_path}")
 
-                # Удаляем старый файл, если он существует
                 if os.path.exists(dest_path):
                     os.remove(dest_path)
 
-                # Перемещаем новый файл на его место
                 shutil.move(src_path, dest_path)
 
-        # Удаляем временные файлы и директорию
         shutil.rmtree(update_dir)
 
-        # Запуск нового процесса в том же терминале
         print("Обновление завершено. Перезапуск бота...")
         os.execv(sys.executable, [sys.executable] + [os.path.join(os.getcwd(), 'main.py')])
 
--- modules/versionChecker.py
+++ modules/versionChecker.py
@@ -1,14 +1,36 @@
 import os
+import platform
 import shutil
 import subprocess
 import sys
 import zipfile
 
 import aiohttp
-import discord
 from discord.ext import commands, tasks
 
 from options import version, servers_data
+
+
+async def git_pull():
+    try:
+        result = subprocess.run(["git", "pull"], cwd=os.getcwd(), capture_output=True, text=True)
+        print(result.stdout)
+        return result.returncode == 0
+    except Exception as e:
+        print(f"Ошибка при выполнении git pull: {e}")
+        return False
+
+
+def restart_bot():
+    try:
+        if platform.system() == "Windows":
+            os.system(f'python "{os.path.join(os.getcwd(), "main.py")}"')
+        else:
+            os.system(f'python3 "{os.path.join(os.getcwd(), "main.py")}"')
+        print("Запущен новый процесс бота.")
+    except Exception as e:
+        print(f"Ошибка при запуске нового процесса: {e}")
+    sys.exit(0)
 
 
 class VersionChecker(commands.Cog):
@@ -28,38 +50,17 @@ class VersionChecker(commands.Cog):
                     data = await response.json()
                     remote_version = data["tag_name"]
                     if self.is_newer_version(remote_version.strip('v')):
-                        if await self.git_pull():
-                            print("Repository updated with git pull.")
+                        print(f"Доступна новая версия бота ({self.current_version} -> {remote_version}). Обновляем...")
+                        if await git_pull():
+                            print("Обновление завершено. Перезапуск бота...")
+                            restart_bot()
                         else:
                             await self.download_and_update(session, data["zipball_url"], remote_version)
-
-    async def git_pull(self):
-        try:
-            result = subprocess.run(["git", "pull"], cwd=os.getcwd(), capture_output=True, text=True)
-            print(result.stdout)
-            return result.returncode == 0
-        except Exception as e:
-            print(f"Ошибка при выполнении git pull: {e}")
-            return False
 
     def is_newer_version(self, remote_version):
         current_version_parts = list(map(int, self.current_version.split('.')))
         remote_version_parts = list(map(int, remote_version.split('.')))
         return remote_version_parts > current_version_parts
-
-    async def notify_new_version(self, new_version):
-        embed = discord.Embed(
-            title="Новая версия бота доступна!",
-            description=f"Текущая версия: **{self.current_version}**\nНовая версия: **{new_version.strip('v')}**",
-            color=discord.Color.green()
-        )
-        for server in self.servers_data:
-            server_data = self.servers_data.get(server)
-            if not server_data:
-                continue
-            channel = self.bot.get_channel(server_data.get("log_channel"))
-            if channel:
-                await channel.send(embed=embed)
 
     async def download_and_update(self, session, url, new_version):
         async with session.get(url) as response:
@@ -69,22 +70,18 @@ class VersionChecker(commands.Cog):
                     file.write(await response.read())
                 base_dir = os.path.join(os.getcwd(), "update")
                 await self.extract_and_replace(zip_path, new_version, base_dir)
-                print(f"Downloaded and extracted {zip_path}")
+                print(f"Загружен и распакован {zip_path}.")
 
     async def extract_and_replace(self, zip_path, new_version, base_dir):
         try:
-            # Папка внутри архива, содержащая файлы проекта
             extract_folder = f"update_{new_version}"
 
-            # Распаковка архива
             with zipfile.ZipFile(zip_path, 'r') as zip_ref:
                 zip_ref.extractall(extract_folder)
 
-            # Получаем имя папки из архива (предполагается, что в архиве только одна папка)
             with zipfile.ZipFile(zip_path, 'r') as zip_ref:
                 folder_name = next(name for name in zip_ref.namelist() if name.endswith('/')).strip('/')
 
-            # Перемещение файлов из распакованной папки в основную директорию
             for root, dirs, files in os.walk(os.path.join(extract_folder, folder_name)):
                 for file in files:
                     src_path = os.path.join(root, file)
@@ -93,35 +90,14 @@ class VersionChecker(commands.Cog):
                     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
                     shutil.move(src_path, dest_path)
 
-            # Удаление временных файлов и директорий
             shutil.rmtree(extract_folder)
             os.remove(zip_path)
 
-            # Перезапуск основного скрипта
-            # self.bot.close()
             subprocess.Popen([sys.executable, "modules/update.py"])
 
         except Exception as e:
             print(f"Ошибка при распаковке и обновлении: {e}")
 
-    async def notify_update(self, new_version):
-        embed = discord.Embed(
-            title="Бот обновлён!",
-            description=f"Бот был обновлён до версии {new_version}.",
-            color=discord.Color.green()
-        )
-        for server in self.servers_data:
-            server_data = self.servers_data.get(server)
-            if not server_data:
-                continue
-            channel = self.bot.get_channel(server_data.get("log_channel"))
-            if channel:
-                await channel.send(embed=embed)
-
     @check_version.before_loop
     async def before_check_version(self):
         await self.bot.wait_until_ready()
-
-    def cog_unload(self):
-        # В данном случае, мы не используем self.session, так как сессия создается и закрывается в check_version и download_and_update
-        pass
